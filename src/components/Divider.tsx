38fa4a38b1da64b07016ce4e661ec0f227f9fce5
Cris <crisnoel12@gmail.com>
2023-12-19 23:13:19 +0800

    fix mb typo

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export default function Divider(props: IProps) {
   const { mt, mb } = props;
   const marginTop = mt ? `mt-${mt}` : "mt-4";
-  const marginBottom = mb ? `mt-${mb}` : null;
+  const marginBottom = mb ? `mb-${mb}` : null;
   const classList = `${marginTop} ${marginBottom}`;
   return <hr className={classList} />;
 }
